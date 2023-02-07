22a52477c0801477d6e46e680b1b03c0eda6097a
ShAd <alexis.tatarkovic@gmail.com>
2023-02-07 11:26:21 +0100

    Commentaires, listes

--- src/main/java/fr/alexisflorent/Main.java
+++ src/main/java/fr/alexisflorent/Main.java
@@ -1,23 +1,28 @@
 package fr.alexisflorent;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Socket socket = null;
         // Boucle de saisie des données
         Scanner scanner = new Scanner(System.in);
+
+        // Création des listes de livres et de lecteurs
         ArrayList<String[]> livres = new ArrayList<>();
         ArrayList<String[]> lecteurs = new ArrayList<>();
+
         while (true) {
             // On affiche le menu
             afficherMenu();
 
-            // Vérification de la saisie
+            // Si l'utilisateur n'a pas saisi un nombre, on lui redemande
             while (!scanner.hasNextInt()) {
                 System.out.print("Veuillez saisir un nombre : ");
                 scanner.nextLine();
@@ -27,28 +32,25 @@ public class Main {
 
             // Traitement de l'option
             if (option == 1) {
-                // Saisie des données
-                System.out.print("Titre : ");
-                String titre = scanner.nextLine();
-                System.out.print("Auteur : ");
-                String auteur = scanner.nextLine();
-                System.out.print("Nombre de pages : ");
-                String nbPages = scanner.nextLine();
+                // Saisie des données du livre
+                String titre = askUser("Titre : ");
+                String auteur = askUser("Auteur : ");
+                String nbPages = askUser("Nombre de pages : ");
                 livres.add(new String[]{titre, auteur, nbPages});
+                System.out.println("Livre ajouté");
             } else if (option == 2) {
-                // Saisie des données
-                System.out.print("Nom : ");
-                String nom = scanner.nextLine();
-                System.out.print("Prénom : ");
-                String prenom = scanner.nextLine();
-                System.out.print("Email : ");
-                String email = scanner.nextLine();
+                // Saisie des données du lecteur
+                String nom = askUser("Nom : ");
+                String prenom = askUser("Prénom : ");
+                String email = askUser("Email : ");
                 lecteurs.add(new String[]{nom, prenom, email});
+                System.out.println("Lecteur ajouté");
             } else if (option == 3) {
                 try {
                     // Connexion et envoie au serveur
                     socket = createSocket();
                     ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    // On ajoute les objets à envoyer
                     oos.writeObject(livres);
                     oos.writeObject(lecteurs);
                     oos.flush();
@@ -59,27 +61,77 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             } else if (option == 4) {
+                try {
+                    // Connexion et envoie au serveur
+                    socket = createSocket();
+                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    // On demande la liste des livres
+                    oos.writeObject("liste");
+                    oos.writeObject(null);
+                    oos.flush();
+
+                    // On récupère la liste des livres que le serveur nous envoie
+                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                    Object listeLivres = ois.readObject();
+                    // On convertit l'object de livres en ArrayList
+                    ArrayList<String[]> listeLivre = (ArrayList<String[]>) listeLivres;
+
+                    System.out.println(" ");
+                    System.out.println("Liste des livres : ");
+                    // Pour chaque livre dans la liste
+                    for (String[] livre : listeLivre) {
+                        System.out.println("ID : " + livre[0] + " -- Titre : " + livre[1] + " -- Auteur : " + livre[2] + " -- Nombre de pages : " + livre[3]);
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (option == 5) {
+                try {
+                    // Connexion et envoie au serveur
+                    socket = createSocket();
+                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    // On demande la liste des livres
+                    oos.writeObject(null);
+                    oos.writeObject("liste");
+                    oos.flush();
+
+                    // On récupère la liste des lecteurs que le serveur nous envoie
+                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                    Object listeLecteurs = ois.readObject();
+                    // On convertit l'object de lecteurs en ArrayList
+                    ArrayList<String[]> listeLecteur = (ArrayList<String[]>) listeLecteurs;
+
+                    System.out.println(" ");
+                    System.out.println("Liste des lecteurs : ");
+                    // Pour chaque lecteur dans la liste
+                    for (String[] lecteur : listeLecteur) {
+                        System.out.println("ID : " + lecteur[0] + " -- Nom : " + lecteur[1] + " -- Prenom : " + lecteur[2] + " -- Email : " + lecteur[3]);
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (option == 6) {
                 break;
             } else {
                 System.out.println("Option invalide");
             }
         }
+        // Déconnexion
+        disconnect(socket);
+    }
 
-        // Déconnexion propre
-        try {
-            if (socket != null) {
-                socket.close();
-            }
-            System.out.println("Déconnecté proprement du serveur");
-        } catch (IOException e) {
-            System.out.println("Erreur lors de la déconnexion");
-            e.printStackTrace();
-        }
+    /**
+     * Demande à l'utilisateur de saisir une valeur
+     * @param message Message à afficher
+     * @return String
+     */
+    private static String askUser(String message) {
+        System.out.println(message);
+        return new Scanner(System.in).nextLine();
     }
 
     /**
      * Affichage du menu
-     * @return void
      */
     private static void afficherMenu() {
         System.out.println(" ");
@@ -88,7 +140,9 @@ public class Main {
         System.out.println("1. Saisir un livre");
         System.out.println("2. Saisir un lecteur");
         System.out.println("3. Envoyer les données au serveur");
-        System.out.println("4. Se déconnecter");
+        System.out.println("4. Afficher la liste des livres");
+        System.out.println("5. Afficher la liste des lecteurs");
+        System.out.println("6. Se déconnecter");
         System.out.print("Votre choix : ");
     }
 
@@ -108,5 +162,20 @@ public class Main {
             e.printStackTrace();
         }
         return socket;
+    }
+
+    /**
+     * Déconnecte proprement le socket
+     * @param socket Socket
+     */
+    private static void disconnect(Socket socket) {
+        // Déconnexion propre
+        try {
+            socket.close();
+            System.out.println("Déconnecté proprement du serveur");
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la déconnexion");
+            e.printStackTrace();
+        }
     }
 }
